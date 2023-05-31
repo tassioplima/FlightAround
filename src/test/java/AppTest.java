9a81e916c0ca7a4e2a2df6156c0c749b2b268858
Tassio Lima <tassiolima@Tassios-MBP.lan>
2023-05-31 18:51:50 +0100

    initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+import appium.Hooks;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import screens.PageNote;
+
+public class AppTest extends Hooks{
+
+    public PageNote pageNote;
+
+    @Test
+    @DisplayName("First Flight")
+    public void newNote() {
+
+    }
+
+}
---
+++ src/test/java/appium/AppiumController.java
@@ -0,0 +1,42 @@
+package appium;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import utils.AndroidCapabilities;
+import utils.Env;
+import utils.iOSCapabilities;
+
+import java.net.URL;
+
+public class AppiumController {
+
+    public static final String server = "https://" + Env.AUTOMATE_USERNAME.getEnv() + ":" + Env.AUTOMATE_ACCESS_KEY.getEnv() + "@hub-cloud.browserstack.com/wd/hub";
+    public static final String local = "http://127.0.0.1:4723/wd/hub";
+
+    public static AppiumDriver driver;
+
+    public void startAppium() throws Exception {
+
+        if (Env.MOBILE.getEnv().equalsIgnoreCase("android")){
+            if (Env.BOOLENV.getBool()){
+                driver = new AppiumDriver<MobileElement>(new URL(server), AndroidCapabilities.getAndroidCapabilities());
+            } else {
+                driver = new AppiumDriver<MobileElement>(new URL(local), AndroidCapabilities.getAndroidCapabilitiesLocal());
+            }
+
+        } else if (Env.MOBILE.getEnv().equalsIgnoreCase("ios")){
+            if (Env.BOOLENV.getBool()){
+                driver = new AppiumDriver<MobileElement>(new URL(server), iOSCapabilities.getIOSCapabilities());
+            } else {
+                driver = new AppiumDriver<MobileElement>(new URL(local), iOSCapabilities.getIOSCapabilitiesLocal());
+            }
+        }
+
+    }
+
+    public void stopAppium() throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
